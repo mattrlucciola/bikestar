bfe9219a0f20d3746bab774620cf47ef9ea0806b
mattrlucciola <mr3lucciola@gmail.com>
2019-10-24 19:16:30 -0400

    finally got heroku bs working

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 app.use('/static',express.static(path.join(__dirname, 'client/build/static')));
 // Handles any requests that don't match the ones above
 app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+    res.sendFile(path.join(__dirname,'/client/build/index.html'));
 });
 
 
