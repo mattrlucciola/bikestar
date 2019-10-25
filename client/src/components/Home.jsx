8dcf74dbfded6be5a4e45d3ce597cd5f34719efc
mattrlucciola <mr3lucciola@gmail.com>
2019-10-25 01:10:04 -0400

    formatting/organizing the css file

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,14 +1,9 @@
 import React, { Component } from 'react';
-// import HikeList from './HikeList'
-// import SearchResults from './SearchResults'
 import axios from 'axios';
 import '../App.css';
 
 //import Map
 import Map from './Map'
-
-
-
 
 class Home extends Component {
     constructor() {
@@ -22,32 +17,27 @@ class Home extends Component {
     }
     async componentDidMount() {
         this.axiosCallByCoordinates()
-
-
     }
 
     axiosCallByCoordinates() {
         if (window.navigator.geolocation) {
             window.navigator.geolocation.getCurrentPosition(async position => {
                 let lat = position.coords.latitude;
-
                 let lon = position.coords.longitude;
-              
                 this.setState({
                     lat: lat,
                     lon: lon
                 })
                 const url = `https://www.mtbproject.com/data/get-trails?lat=${this.state.lat}&lon=${this.state.lon}&maxResults=10&key=200602842-d85aaa22cf9dc2a1ba17569b62a04f4e`
                 const response = await axios(url)
-                console.log(response.data.trails)
                 this.setState({
                     trails: response.data.trails,
-                })
-            }, 
+                })}, 
                 error => {
                     console.log("Error:", error)
                 },
-                { enableHighAccuracy: true })
+                { enableHighAccuracy: true }
+            )
         }
     }
 
@@ -55,18 +45,15 @@ class Home extends Component {
         this.setState({
             trailId: trailId
         })
-    
-        console.log(trailId)
     }
 
     parseSearch = async (lat, lon) =>{
-        console.log(lat, lon);
         const url = `https://www.mtbproject.com/data/get-trails?lat=${lat}&lon=${lon}&maxResults=10&key=200602842-d85aaa22cf9dc2a1ba17569b62a04f4e`
-                const response = await axios(url)
-                console.log(response.data.trails)
-                this.setState({
-                    trails: response.data.trails,
-                })
+        const response = await axios(url)
+        console.log(response.data.trails)
+        this.setState({
+            trails: response.data.trails,
+        })
     }
 
     render() {
@@ -81,5 +68,3 @@ class Home extends Component {
 }
 
 export default Home;
-
-// info={trailObj} model={'trail'} 
--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,29 +1,22 @@
 import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
 import Search from './Search';
-import BurgerMenu from './BurgerMenu';
+import BurgerMenu from './BurgerMenu.jsx';
 
 export default function Nav({searchChange, search, useSearch}){
-    // console.log(searchChange, search);
-
     let [isOpen,setIsOpen] = useState(false)
-
-    const clickHandler = () => {
-        setIsOpen(!isOpen)        
-    }
+    const clickHandler = () => {setIsOpen(!isOpen)}
 
     return(
         <div className='nav'>
             <div className="links">
-            <div className="bike-star">
-            <Link to='/'>BikeStar</Link>
-            </div>
-            <div className="state-trail">
-                <Link to='/states'>States</Link>
-                <Link to='/trails'>Trails</Link>
-            </div>
-                {/* <Link to=  >Bur Menu</Link> */}
-                {/* <BurgerMenu /> */}
+                <div className="bike-star">
+                    <Link to='/'>BikeStar</Link>
+                </div>
+                <div className="state-trail">
+                    <Link to='/states'>States</Link>
+                    <Link to='/trails'>Trails</Link>
+                </div>
                 <div className="user-menu" onClick={clickHandler}>
                     Users &#9776;
                     {isOpen && <BurgerMenu />}
